8c78aeb48bc3338d91ad04df9169b62cc3cf6621
Tom Beardsley <89028856+tmbeardsley@users.noreply.github.com>
2024-01-29 21:15:39 -0500

    Update fts_gpu.cu
    Updated comments

--- fts_gpu.cu
+++ fts_gpu.cu
@@ -1,3 +1,10 @@
+// ########################################################################
+// Creates a new wtmd_simulation(...) object, passing input files to 
+// its contructor and subsequently accessing its public methods to 
+// equilibrate the system, gather statistics from the equilibrated system,
+// and finally output the system's energy
+// ########################################################################
+
 #include <stdlib.h>
 #include <iostream>
 #include "wtmd_simulation.h"
@@ -8,18 +15,22 @@ int main ()
 {
     time_t t;
 
+    // New wtmd_simulation object with input file name specified
     wtmd_simulation *wtmd_sim = new wtmd_simulation("input", "bias_in");
-    
+
+    // Time the equilibration period
     cout << "Starting Equilibrating..." << endl;
     t = time(NULL);
     wtmd_sim -> equilibrate();
     cout << "Equlibration time = " << time(NULL)-t << "secs" << endl << endl;
 
+    // Time the statistics gathering period
     cout << "Starting Statistics..." << endl;
     t = time(NULL);
     wtmd_sim -> statistics();
     cout << "Statistics time = " << time(NULL)-t << "secs" << endl << endl;
 
+    // Output the final energy of the system
     cout.precision(6);
     cout << wtmd_sim->getH() << endl;
 
