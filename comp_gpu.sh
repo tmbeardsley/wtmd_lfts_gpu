43776dfec302401e005057a01664a9dadc63df9c
Tom Beardsley <t.beardsley@hotmail.co.uk>
2024-10-09 20:01:16 -0400

    Updated comp.sh

--- comp_gpu.sh
+++ comp_gpu.sh
@@ -3,4 +3,4 @@
 
 mkdir ./build
 
-nvcc ./src/fts_gpu.cu -o ./build/wtmd-fts-gpu -O2 -std=c++14 -lcufft -lgsl -lgslcblas -lm -lcurand -gencode=arch=compute_52,code=sm_52 -gencode=arch=compute_60,code=sm_60 -gencode=arch=compute_61,code=sm_61 -gencode=arch=compute_70,code=sm_70 -gencode=arch=compute_75,code=sm_75 -gencode=arch=compute_80,code=sm_80 -gencode=arch=compute_80,code=compute_80
+nvcc ./src/wtmd_lfts_gpu.cu -o ./build/wtmd-lfts-gpu -O2 -std=c++14 -lcufft -lgsl -lgslcblas -lm -lcurand -gencode=arch=compute_52,code=sm_52 -gencode=arch=compute_60,code=sm_60 -gencode=arch=compute_61,code=sm_61 -gencode=arch=compute_70,code=sm_70 -gencode=arch=compute_75,code=sm_75 -gencode=arch=compute_80,code=sm_80 -gencode=arch=compute_80,code=compute_80
